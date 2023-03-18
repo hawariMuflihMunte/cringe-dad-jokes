0d0b8bcc0d1130d0d70f394b09aba72ffff56acc
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-18 10:57:33 +0700

    Disable Bundle Analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+// const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 module.exports = {
 	mode: 'development',
@@ -56,10 +56,10 @@ module.exports = {
 			filename: 'index.html',
 			template: 'src/template.html',
 		}),
-		new BundleAnalyzerPlugin({
-			analyzerHost: 'localhost',
-			analyzerPort: 3100,
-			openAnalyzer: false,
-		}),
+		//new BundleAnalyzerPlugin({
+			//analyzerHost: 'localhost',
+			//analyzerPort: 3100,
+			//openAnalyzer: false,
+		//}),
 	],
 };
